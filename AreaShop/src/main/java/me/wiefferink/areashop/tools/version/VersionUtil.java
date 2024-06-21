5034aa358ab5726351e971a5e95e43b01ace5470
Andrew Wong <42793301+md5sha256@users.noreply.github.com>
2024-06-22 01:22:55 +1000

    Fix version util parsing for versions without trailing zeros

--- AreaShop/src/main/java/me/wiefferink/areashop/tools/version/VersionUtil.java
+++ AreaShop/src/main/java/me/wiefferink/areashop/tools/version/VersionUtil.java
@@ -1,10 +1,29 @@
 package me.wiefferink.areashop.tools.version;
 
+import javax.annotation.Nonnull;
+import java.util.StringJoiner;
+
 public class VersionUtil {
 
     public static final VersionData MC_1_20_6 = new VersionData(1, 20, 6);
 
-    public static Version parseMinecraftVersion(String minecraftVersion) {
+    @Nonnull
+    public static String padTrailingZero(@Nonnull String minecraftVersion) {
+        int dotCount = 0;
+        for (char c : minecraftVersion.toCharArray()) {
+            if (c == '.') {
+                dotCount += 1;
+            }
+        }
+        StringJoiner joiner = new StringJoiner("");
+        for (int i = 0; i < 2 - dotCount; i++) {
+            joiner.add(".0");
+        }
+        return minecraftVersion + joiner;
+    }
+
+    @Nonnull
+    public static Version parseMinecraftVersion(@Nonnull String minecraftVersion) {
         // Expecting 1.X.X-R0.1-SNAPSHOT
         int stripLength = "-R0.1-SNAPSHOT".length();
         int length = minecraftVersion.length();
@@ -13,7 +32,7 @@ public class VersionUtil {
         }
         String strippedVersion = minecraftVersion.substring(0, length - stripLength);
         try {
-            return Version.parse(strippedVersion);
+            return Version.parse(padTrailingZero(strippedVersion));
         } catch (IllegalArgumentException ex) {
             throw new IllegalArgumentException("Invalid minecraft version: " + minecraftVersion, ex);
         }
