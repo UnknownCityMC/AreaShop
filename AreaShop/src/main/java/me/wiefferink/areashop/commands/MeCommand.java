b631cfc89daf4e3e5077a39e831a1a716d4a5d8a
Andrew Wong <42793301+md5sha256@users.noreply.github.com>
2024-08-04 21:10:04 +1000

    Remove argument from me command

--- AreaShop/src/main/java/me/wiefferink/areashop/commands/MeCommand.java
+++ AreaShop/src/main/java/me/wiefferink/areashop/commands/MeCommand.java
@@ -3,22 +3,18 @@ package me.wiefferink.areashop.commands;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 import me.wiefferink.areashop.MessageBridge;
-import me.wiefferink.areashop.adapters.platform.OfflinePlayerHelper;
 import me.wiefferink.areashop.commands.util.AreaShopCommandException;
 import me.wiefferink.areashop.commands.util.AreashopCommandBean;
-import me.wiefferink.areashop.commands.util.OfflinePlayerParser;
 import me.wiefferink.areashop.commands.util.RegionInfoUtil;
 import me.wiefferink.areashop.commands.util.commandsource.CommandSource;
 import me.wiefferink.areashop.commands.util.commandsource.PlayerCommandSource;
 import me.wiefferink.areashop.managers.IFileManager;
-import me.wiefferink.areashop.tools.BukkitSchedulerExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.incendo.cloud.Command;
 import org.incendo.cloud.bean.CommandProperties;
 import org.incendo.cloud.context.CommandContext;
-import org.incendo.cloud.key.CloudKey;
 import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nonnull;
@@ -26,23 +22,16 @@ import javax.annotation.Nonnull;
 @Singleton
 public class MeCommand extends AreashopCommandBean {
 
-    private static final CloudKey<String> KEY_PLAYER = CloudKey.of("player", String.class);
     private final IFileManager fileManager;
     private final MessageBridge messageBridge;
-    private final OfflinePlayerHelper offlinePlayerHelper;
-    private final BukkitSchedulerExecutor executor;
 
     @Inject
     public MeCommand(
             @Nonnull MessageBridge messageBridge,
-            @Nonnull IFileManager fileManager,
-            @Nonnull OfflinePlayerHelper offlinePlayerHelper,
-            @Nonnull BukkitSchedulerExecutor executor
+            @Nonnull IFileManager fileManager
     ) {
         this.messageBridge = messageBridge;
         this.fileManager = fileManager;
-        this.offlinePlayerHelper = offlinePlayerHelper;
-        this.executor = executor;
     }
 
     @Override
@@ -54,7 +43,6 @@ public class MeCommand extends AreashopCommandBean {
     protected Command.Builder<? extends CommandSource<?>> configureCommand(Command.@NotNull Builder<CommandSource<?>> builder) {
         return builder.literal("me")
                 .senderType(PlayerCommandSource.class)
-                .optional(KEY_PLAYER, OfflinePlayerParser.parser())
                 .handler(this::handleCommand);
     }
 
@@ -76,19 +64,7 @@ public class MeCommand extends AreashopCommandBean {
         if (!sender.hasPermission("areashop.me")) {
             throw new AreaShopCommandException("me-noPermission");
         }
-        this.offlinePlayerHelper.lookupOfflinePlayerAsync(context.get(KEY_PLAYER))
-                .whenCompleteAsync((offlinePlayer, exception) -> {
-                    if (exception != null) {
-                        sender.sendMessage("failed to lookup offline player!");
-                        exception.printStackTrace();
-                        return;
-                    }
-                    if (!offlinePlayer.hasPlayedBefore()) {
-                        this.messageBridge.message(sender, "cmd-invalidPlayer", offlinePlayer.getName());
-                        return;
-                    }
-                    RegionInfoUtil.showRegionInfo(this.messageBridge, this.fileManager, sender, offlinePlayer);
-                }, this.executor);
+        RegionInfoUtil.showRegionInfo(this.messageBridge, this.fileManager, sender, sender);
     }
 
 }
